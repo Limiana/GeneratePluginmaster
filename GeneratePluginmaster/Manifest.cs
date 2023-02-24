71c6c673e16fa323e5095dd3b37725099a02e2aa
Limiana <5073202+Limiana@users.noreply.github.com>
2023-02-24 05:33:23 +0300

    Add project files.

---
+++ GeneratePluginmaster/Manifest.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Dalamud.Game;
+using Newtonsoft.Json;
+
+namespace Dalamud.Plugin.Internal.Types;
+
+/// <summary>
+/// Information about a plugin, packaged in a json file with the DLL.
+/// </summary>
+internal class PluginManifest
+{
+    /// <summary>
+    /// Gets the author/s of the plugin.
+    /// </summary>
+    [JsonProperty]
+    public string? Author { get; set; }
+
+    /// <summary>
+    /// Gets or sets the public name of the plugin.
+    /// </summary>
+    [JsonProperty]
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets a punchline of the plugins functions.
+    /// </summary>
+    [JsonProperty]
+    public string? Punchline { get; set; }
+
+    /// <summary>
+    /// Gets a description of the plugins functions.
+    /// </summary>
+    [JsonProperty]
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets a changelog.
+    /// </summary>
+    [JsonProperty]
+    public string? Changelog { get; set; }
+
+    /// <summary>
+    /// Gets a list of tags defined on the plugin.
+    /// </summary>
+    [JsonProperty]
+    public List<string>? Tags { get; set; }
+
+    /// <summary>
+    /// Gets a list of category tags defined on the plugin.
+    /// </summary>
+    [JsonProperty]
+    public List<string>? CategoryTags { get; set; }
+
+    /// <summary>
+    /// Gets a value indicating whether or not the plugin is hidden in the plugin installer.
+    /// This value comes from the plugin master and is in addition to the list of hidden names kept by Dalamud.
+    /// </summary>
+    [JsonProperty]
+    public bool IsHide { get; set; }
+
+    /// <summary>
+    /// Gets the internal name of the plugin, which should match the assembly name of the plugin.
+    /// </summary>
+    [JsonProperty]
+    public string InternalName { get; set; } = null!;
+
+    /// <summary>
+    /// Gets the current assembly version of the plugin.
+    /// </summary>
+    [JsonProperty]
+    public Version AssemblyVersion { get; set; } = null!;
+
+    /// <summary>
+    /// Gets the current testing assembly version of the plugin.
+    /// </summary>
+    [JsonProperty]
+    public Version? TestingAssemblyVersion { get; set; }
+
+    /// <summary>
+    /// Gets a value indicating whether the plugin is only available for testing.
+    /// </summary>
+    [JsonProperty]
+    public bool IsTestingExclusive { get; set; }
+
+    /// <summary>
+    /// Gets an URL to the website or source code of the plugin.
+    /// </summary>
+    [JsonProperty]
+    public string? RepoUrl { get; set; }
+
+    /// <summary>
+    /// Gets the version of the game this plugin works with.
+    /// </summary>
+    [JsonProperty]
+    [JsonConverter(typeof(GameVersionConverter))]
+    public GameVersion? ApplicableVersion { get; set; } = GameVersion.Any;
+
+    /// <summary>
+    /// Gets the API level of this plugin. For the current API level, please see <see cref="PluginManager.DalamudApiLevel"/>
+    /// for the currently used API level.
+    /// </summary>
+    [JsonProperty]
+    public int DalamudApiLevel { get; set; } = 8;
+
+    /// <summary>
+    /// Gets the number of downloads this plugin has.
+    /// </summary>
+    [JsonProperty]
+    public long DownloadCount { get; set; }
+
+    /// <summary>
+    /// Gets the last time this plugin was updated.
+    /// </summary>
+    [JsonProperty]
+    public long LastUpdate { get; set; }
+
+    /// <summary>
+    /// Gets the download link used to install the plugin.
+    /// </summary>
+    [JsonProperty]
+    public string DownloadLinkInstall { get; set; } = null!;
+
+    /// <summary>
+    /// Gets the download link used to update the plugin.
+    /// </summary>
+    [JsonProperty]
+    public string DownloadLinkUpdate { get; set; } = null!;
+
+    /// <summary>
+    /// Gets the download link used to get testing versions of the plugin.
+    /// </summary>
+    [JsonProperty]
+    public string DownloadLinkTesting { get; set; } = null!;
+
+    /// <summary>
+    /// Gets the required Dalamud load step for this plugin to load. Takes precedence over LoadPriority.
+    /// Valid values are:
+    /// 0. During Framework.Tick, when drawing facilities are available.
+    /// 1. During Framework.Tick.
+    /// 2. No requirement.
+    /// </summary>
+    [JsonProperty]
+    public int LoadRequiredState { get; set; }
+
+    /// <summary>
+    /// Gets a value indicating whether Dalamud must load this plugin not at the same time with other plugins and the game.
+    /// </summary>
+    [JsonProperty]
+    public bool LoadSync { get; set; }
+
+    /// <summary>
+    /// Gets the load priority for this plugin. Higher values means higher priority. 0 is default priority.
+    /// </summary>
+    [JsonProperty]
+    public int LoadPriority { get; set; }
+
+    /// <summary>
+    /// Gets a value indicating whether the plugin can be unloaded asynchronously.
+    /// </summary>
+    [JsonProperty]
+    public bool CanUnloadAsync { get; set; }
+
+    /// <summary>
+    /// Gets a list of screenshot image URLs to show in the plugin installer.
+    /// </summary>
+    public List<string>? ImageUrls { get; set; }
+
+    /// <summary>
+    /// Gets an URL for the plugin's icon.
+    /// </summary>
+    public string? IconUrl { get; set; }
+
+    /// <summary>
+    /// Gets a value indicating whether this plugin accepts feedback.
+    /// </summary>
+    public bool AcceptsFeedback { get; set; } = true;
+
+    /// <summary>
+    /// Gets a message that is shown to users when sending feedback.
+    /// </summary>
+    public string? FeedbackMessage { get; set; }
+}
---
+++ GeneratePluginmaster/Program.cs
@@ -0,0 +1,166 @@
+﻿using Dalamud.Plugin.Internal.Types;
+using Newtonsoft.Json;
+using System.IO.Compression;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+
+internal class Program
+{
+    const string BaseUrl = "https://love.puni.sh/plugins/";
+    private static void Main(string[] args)
+    {
+        if(args.Length == 0)
+        {
+            Console.WriteLine($"No arguments specified, assuming current folder contains plugins folder");
+            Process(".");
+        }
+        else
+        {
+            Process(args[0]);
+        }
+    }
+
+    static void Process(string folderPath)
+    {
+        var path = Path.Combine(folderPath, $"plugins");
+        Console.WriteLine($"Plugins folder: {path}");
+        if (!Directory.Exists(path))
+        {
+            Console.WriteLine($"Folder does not exists");
+            Environment.Exit(0);
+        }
+        List<PluginManifest> Manifests = new();
+        foreach(var f in Directory.GetDirectories(path))
+        {
+            var m = ProcessPlugin(f);
+            if(m != null)
+            {
+                Manifests.Add(m);
+            }
+        }
+        var mf = JsonConvert.SerializeObject(Manifests, Formatting.Indented, new JsonSerializerSettings()
+        {
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+        });
+        Console.WriteLine(mf);
+        File.WriteAllText($"ment.json", mf);
+    }
+
+    static PluginManifest? ProcessPlugin(string path)
+    {
+        Console.WriteLine($"Processing {path}");
+        if (!Directory.Exists(path))
+        {
+            Console.WriteLine("Folder doesn't exists");
+            return null;
+        }
+        var file = Path.Combine(path, "latest.zip");
+        var testing = Path.Combine(path, "Testing", "latest.zip");
+        if (!File.Exists(file))
+        {
+            Console.WriteLine("Can't find latest.zip");
+            return null;
+        }
+        try
+        {
+            var manifest = GetMainfest(file);
+            if(manifest == null)
+            {
+                Console.WriteLine("Could not get plugin manifest");
+                return null;
+            }
+            manifest.DownloadLinkInstall = $"{BaseUrl}{new DirectoryInfo(path).Name}/latest.zip";
+            manifest.DownloadLinkUpdate = manifest.DownloadLinkInstall;
+            manifest.DownloadLinkTesting = manifest.DownloadLinkInstall;
+            if (File.Exists(testing))
+            {
+                Console.WriteLine("Now processing testing version");
+                var testingManifest = GetMainfest(testing);
+                if(testingManifest != null)
+                {
+                    manifest.DownloadLinkTesting = $"{BaseUrl}{new DirectoryInfo(path).Name}/Testing/latest.zip";
+                    manifest.TestingAssemblyVersion = testingManifest.AssemblyVersion;
+                }
+                else
+                {
+                    Console.WriteLine("Could not get testing plugin manifest");
+                    manifest.DownloadLinkTesting = manifest.DownloadLinkInstall;
+                }
+            }
+            else
+            {
+                Console.WriteLine("No testing version for this plugin");
+            }
+            return manifest;
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine($"{e.Message}\n{e.StackTrace}");
+        }
+        return null;
+    }
+
+    static PluginManifest? GetMainfest(string file)
+    {
+        using ZipArchive zip = ZipFile.OpenRead(file);
+        foreach (ZipArchiveEntry entry in zip.Entries)
+        {
+            if (entry.FullName.EndsWith(".dll"))
+            {
+                try
+                {
+                    Console.WriteLine($"Inspecting {entry.FullName}");
+                    using var sr = new MemoryStream(ReadStream(entry.Open()));
+                    using var portableExecutableReader = new PEReader(sr);
+                    var metadataReader = portableExecutableReader.GetMetadataReader();
+
+                    foreach (var typeDefHandle in metadataReader.TypeDefinitions)
+                    {
+                        var typeDef = metadataReader.GetTypeDefinition(typeDefHandle);
+                        var name = metadataReader.GetString(typeDef.Name);
+                        //Console.WriteLine($"Checking {name}");
+                        var interfaceHandles = typeDef.GetInterfaceImplementations();
+
+                        foreach (var interfaceHandle in interfaceHandles)
+                        {
+                            var interfaceImplementation = metadataReader.GetInterfaceImplementation(interfaceHandle);
+                            var interfaceType = interfaceImplementation.Interface;
+
+                            if (interfaceType.Kind == HandleKind.TypeReference)
+                            {
+                                var typeReferenceHandle = (TypeReferenceHandle)interfaceType;
+                                var typeReference = metadataReader.GetTypeReference(typeReferenceHandle);
+                                var namespaceName = metadataReader.GetString(typeReference.Namespace);
+                                var interfaceName = metadataReader.GetString(typeReference.Name);
+                                if ($"{namespaceName}.{interfaceName}" == "Dalamud.Plugin.IDalamudPlugin")
+                                {
+                                    var jsonF = $"{entry.FullName[..^4]}.json";
+                                    Console.WriteLine($"Found plugin: {entry.FullName}, loading {jsonF}");
+                                    using var reader = new StreamReader(zip.Entries.First(x => x.FullName == jsonF).Open());
+                                    var json = JsonConvert.DeserializeObject<PluginManifest>(reader.ReadToEnd());
+                                    return json;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Can't load {entry.FullName}: {ex.Message}");
+                }
+            }
+        }
+        return null;
+    }
+
+    static byte[] ReadStream(Stream sourceStream)
+    {
+        using (var memoryStream = new MemoryStream())
+        {
+            sourceStream.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
+    }
+}
